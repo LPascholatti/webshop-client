5d8591310d444f71bd5f12d0a1cc1ca3ed6b9c84
LPascholatti <lucaspascholatti@gmail.com>
2019-10-02 19:03:40 +0200

    Added an animated GIF in the footer

---
+++ src/components/Gif.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Gif extends React.Component {
+  render () {
+    return (
+      <img src="https://media.giphy.com/media/d3mmdNnW5hkoUxTG/giphy.gif" alt="Gif"/>
+    )
+  }
+}
+
+export default Gif;

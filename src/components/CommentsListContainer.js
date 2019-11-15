38013b846dcb30d4ee5a0aa6e4c8d215e67028fb
LPascholatti <lucaspascholatti@gmail.com>
2019-11-15 14:21:44 +0100

    Code in commment list container and component

--- src/components/CommentsListContainer.js
+++ src/components/CommentsListContainer.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react'
+import { loadComments } from '../actions'
+import { connect } from 'react-redux'
+import CommentsList from './CommentsList'
 
-export default class CommentsListContainer extends Component {
+class CommentsListContainer extends Component {
+
+  componentDidMount() {
+    this.props.loadComments()
+  }
+
   render() {
-    return (
-      <div>
-        
-      </div>
-    )
+    console.log("this.props.comments:", this.props.comments)
+    return <CommentsList comments={this.props.comments} product={this.props.product} />
   }
 }
+
+const mapStateToProps = state => ({
+  comments: state.comments,
+  product: state.product
+})
+
+export default connect(mapStateToProps, {loadComments})(CommentsListContainer)

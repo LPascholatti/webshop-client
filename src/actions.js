9d80e5dea45320b986987471cd876f361ed4288d
LPascholatti <lucaspascholatti@gmail.com>
2019-12-03 15:43:50 +0100

    Commit Heroku server in url at actions

--- src/actions.js
+++ src/actions.js
@@ -11,7 +11,7 @@ export const LOGGED_EMAIL = 'LOGGED_EMAIL'
 export const COMMENTS_FETCHED = 'COMMENTS_FETCHED'
 export const COMMENT_CREATE = 'COMMENT_CREATE'
 
-const baseUrl = 'http://localhost:4000'
+const baseUrl = 'https://guarded-citadel-28026.herokuapp.com/'
 
 const productsFetched = payload => ({
   type: PRODUCTS_FETCHED,
